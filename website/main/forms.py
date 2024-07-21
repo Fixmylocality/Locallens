fd0355ce93e517f0792ec58b9e221cce8c8bceee
Fixmylocality <fixmylocality@gmail.com>
2024-07-21 06:29:54 +0530

    Delete website directory

--- website/main/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Problem
-
-class ProblemForm(forms.ModelForm):
-    class Meta:
-        model = Problem
-        fields = ['locality', 'problem_type', 'description', 'image', 'geo_location']
